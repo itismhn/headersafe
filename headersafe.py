c1b3bc8c593ba3c1c104876cce822a9382a7df95
Mohammad Hossein Nouri <mmdnoori13800@gmail.com>
2025-04-27 11:14:07 +0330

    add List of Important security HTTP Headers

--- headersafe.py
+++ headersafe.py
@@ -1,1 +1,21 @@
 import requests
+
+SECURITY_HEADERS = [
+    "Content-Security-Policy",
+    "Strict-Transport-Security",
+    "X-Content-Type-Options",
+    "X-Frame-Options",
+    "Referrer-Policy",
+    "Permissions-Policy",
+    "X-XSS-Protection",
+    "Cross-Origin-Resource-Policy",
+    "Cross-Origin-Embedder-Policy",
+    "Cross-Origin-Opener-Policy",
+    "Cache-Control",
+    "Pragma",
+    "Access-Control-Allow-Origin",
+    "Access-Control-Allow-Methods",
+    "Access-Control-Allow-Headers",
+    "Server",
+    "Set-Cookie",
+]
