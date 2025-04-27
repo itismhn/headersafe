50d2a462810960e4d2c4aa58492ac486f393dd52
Mohammad Hossein Nouri <mmdnoori13800@gmail.com>
2025-04-27 11:24:35 +0330

    check connection to URL

--- headersafe.py
+++ headersafe.py
@@ -21,7 +21,16 @@ SECURITY_HEADERS = [
 ]
 
 def check_security_headers(url):
-    print(url)
+    try:
+        try:
+            response = requests.head(url, allow_redirects=True, timeout=10)
+            if not response.headers:
+                raise Exception("Empty headers with HEAD request.")
+        except:
+            print("[*] HEAD request failed or returned no headers. Trying GET...")
+            response = requests.get(url, allow_redirects=True, timeout=10)
+    except requests.exceptions.RequestException as e:
+        print(f"[!] Error connecting to {url}: {e}")
 
 if __name__ == "__main__":
     target_url = input("Enter the URL (include http/https): ").strip()
