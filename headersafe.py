7165df3011c4ed86c4be7eb2bd71dffbab1ea2e9
Mohammad Hossein Nouri <mmdnoori13800@gmail.com>
2025-04-27 10:32:41 +0330

    remove first version

--- headersafe.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-sample_url="https://tfas.ir"
-def check_headers(url):
-    response=requests.get(url)
-    headers=response.headers
-    print(headers)
-
-check_headers(sample_url)
